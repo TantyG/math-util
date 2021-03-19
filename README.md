7f83cfced24218af981f77c9ff7d21193e8e947f
TantyG <daoty2512@gmail.com>
2021-03-20 06:49:21 +0700

    Add full of README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 ## Welcome to Math-Utility Respository. This repo offers various math methods used as a helper for your project.
+
+
+### _You will find in this repo the following stuff:_
+
+* .gitignore itself to demonstrate how Git too will upload a file or not
+* README.md itself file to introduce the contents of this repo
+* All of math methos to support for the math calculation in your project, e.g. getFactorial(), sin(), cos()
+* (TBA...)
+
+
+### Connect me via:
+[My facebook](http://facebook.com)  
+[My email](daoty2512@gmail.com)
+
+© 2021 tanty
---
+++ src/com/ty/MathUtility.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ty;
+
+/**
+ *
+ * @author DELL
+ */
+public class MathUtility {
+    //class nay chua 1 nhom hang toan hoc, fake tu class chuan
+    //java.until.Math cuar JDK
+    //vi la do xai chung cho nen se l aSTATIC
+    public static final double PI = 3.1415;
+    
+    //ham tinh n!
+    public static long getFactorial(int n){
+        return 0;
+    }
+    
+}
