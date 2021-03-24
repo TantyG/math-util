03ed43196423b3c10a9bcf7882d20237cebf2143
TantyG <daoty2512@gmail.com>
2021-03-24 08:29:28 +0700

    GREEN

--- src/com/ty/mathutil/MathUtility.java
+++ src/com/ty/mathutil/MathUtility.java
@@ -30,7 +30,7 @@ public class MathUtility {
             return 1;
 
         //song sot den doan nay, sure n = 0..20
-        long result = 0; //co tinh de value nay
+        long result = 1; //co tinh de value nay
         for (int i = 2; i <= n; i++) 
             result *= i;
         //n! = 1.2.3.4.5....n
